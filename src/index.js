cae23223b60a3bbe221b94fab3f9ee4fe8670b56
Uladzimir <berkut2430@gmail.com>
2018-03-03 16:56:07 +0300

    eleven

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 module.exports = function count(s, pairs) {
   let N=1, arr=[], count=0;
-
   for (let i=0, prs=pairs.length; i<prs; i++) {
     N*=pairs[i][0]**pairs[i][1];
   }
-
-	if(N > 10000007) return count;  //can't solve yet for N > 1000000
-
+	if(N>10000007) return count;
 	for(let k=1; k<=N; k++) {
 		for(let j=0, sl=s.length; j<sl; j++) {
       if(s[j]=='1') {evklid(k+j, N)==1?arr.push(true):arr.push(false);
       }	else if(s[j]=='0') {evklid(k+j, N)!==1?arr.push(true):arr.push(false);}
 		}
-    if(arr[arr.length-1]==true) {count++;
-    }
+		if(logic(arr)) count++;
 		arr.length=0;
 	}
 	return count;
+}
+
+let logic= function(arr) {
+  let bul=true;
+	for(let i=0, al=arr.length; i<al; i++) {
+		bul=bul && arr[i];
+	}
+	return bul;
 }
 
 let evklid = function(a, b) {
