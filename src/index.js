edad2f4c9d6e58bf4cc73adb8efa57b857ed61a7
Uladzimir <berkut2430@gmail.com>
2018-03-05 20:13:46 +0300

    64%

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 module.exports = function count(s, pairs) {
-  let N=1, arr=[], count=0;
+  let N=1, arr=[], count=0, sum=1;
   for (let i=0, prs=pairs.length; i<prs; i++) {
-    N*=pairs[i][0]**pairs[i][1];
+    N*=pairs[i][0];
+    if (pairs[i][1]!=1) {sum=pairs[i][0]**(pairs[i][1]-1);}
   }
-	if(N>10000007) return count;
+	if(N>100000000) {return count;}
 	for(let k=1; k<=N; k++) {
 		for(let j=0, sl=s.length; j<sl; j++) {
       if(s[j]=='1') {evklid(k+j, N)==1?arr.push(true):arr.push(false);
       }	else if(s[j]=='0') {evklid(k+j, N)!==1?arr.push(true):arr.push(false);}
 		}
-		if(logic(arr)) count++;
+		if(logic(arr)==true) {count++;}
 		arr.length=0;
 	}
-	return count;
+	return count*sum;
 }
 
-let logic= function(arr) {
+let logic = function(arr) {
   let bul=true;
 	for(let i=0, al=arr.length; i<al; i++) {
 		bul=bul && arr[i];
