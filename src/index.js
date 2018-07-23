ef33a6407a074034018bb4e8b599518d851024a7
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-23 23:04:02 +0300

    code improved

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 module.exports = function count(s, pairs) {
-  let N=1, arr=[], count=0, sum=1;
-  for (let i=0, prs=pairs.length; i<prs; i++) {
-    N*=pairs[i][0];
-    if (pairs[i][1]!=1) {sum=pairs[i][0]**(pairs[i][1]-1);}
+  let N = 1, arr = [], count = 0, sum = 1;
+  for (let i = 0, prs = pairs.length; i < prs; i++) {
+    N *= pairs[i][0];
+    if (pairs[i][1] !== 1) {sum = pairs[i][0] ** (pairs[i][1] - 1);}
   }
-	if(N>100000000) {return count;}
-	for(let k=1; k<=N; k++) {
-		for(let j=0, sl=s.length; j<sl; j++) {
-      if(s[j]=='1') {evklid(k+j, N)==1?arr.push(true):arr.push(false);
-      }	else if(s[j]=='0') {evklid(k+j, N)!==1?arr.push(true):arr.push(false);}
+	if(N > 100000000) {return count;}
+	for(let k = 1; k <= N; k++) {
+		for(let j = 0, sl = s.length; j < sl; j++) {
+      if(s[j] === '1') {evklid(k + j, N) === 1 ? arr.push(true) : arr.push(false);
+      }	else if(s[j] === '0') {evklid(k + j, N) !== 1 ? arr.push(true) : arr.push(false);}
 		}
-		if(logic(arr)==true) {count++;}
-		arr.length=0;
+		if(logic(arr) === true) {count++;}
+		arr.length = 0;
 	}
-	return count*sum;
+	return count * sum;
 }
 
 let logic = function(arr) {
-  let bul=true;
-	for(let i=0, al=arr.length; i<al; i++) {
-		bul=bul && arr[i];
+  let bul = true;
+	for(let i = 0, al = arr.length; i < al; i++) {
+		bul = bul && arr[i];
 	}
 	return bul;
 }
 
 let evklid = function(a, b) {
   while (true) {
-    if (b==0) return a;
-    a%=b;
-    if (a==0) return b;
-    b%=a;
+    if (b === 0) return a;
+    a %= b;
+    if (a === 0) return b;
+    b %= a;
   }
 }
